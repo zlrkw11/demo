a3aba45b91845637dc42e5f91faae300e2b363ae
ZL <ray040406@gmail.com>
2025-12-31 16:06:57 +1300

    first commit

---
+++ src/main/java/com/invest/demo/Controller.java
@@ -0,0 +1,9 @@
+package com.invest.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+public class Controller {
+
+}
