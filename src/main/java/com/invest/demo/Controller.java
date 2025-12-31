a022d38c453d021e0e0f8f8991f86d55b81405b8
ZL <ray040406@gmail.com>
2025-12-31 16:30:27 +1300

    feat: running sprinboot application firsttime

--- src/main/java/com/invest/demo/Controller.java
+++ src/main/java/com/invest/demo/Controller.java
@@ -1,9 +1,14 @@
 package com.invest.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class Controller {
-
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
 }
