6e30d0d8fc2f3e038881a5083802976f3eff83cf
ZL <ray040406@gmail.com>
2026-01-02 20:03:36 +1300

    feat: exercise entity

--- src/main/java/com/invest/demo/model/Exercise.java
+++ src/main/java/com/invest/demo/model/Exercise.java
@@ -1,5 +1,26 @@
 package com.invest.demo.model;
 
-public class Exercise {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
+@Entity
+public class Exercise {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String muscleGroup;
+
+    @Column(nullable = false)
+    private int sets;
+
+    @Column(nullable = false)
+    private int reps;
 }
